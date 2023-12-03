331d57956eb6b41904a9060e55815f35ff0f1883
Inês Almeida <britalmeida@gmail.com>
2023-12-03 08:31:28 +0100

    Convert individual arrays to presentSimData array
    - re-order code as in the campaign
    - generate presents either from provided data or hardcoded test setup

--- src/main.ts
+++ src/main.ts
@@ -3,42 +3,51 @@ import { UIRenderer, vec2, vec4, hexToRGBFloat } from './shading';
 
 import { simData } from "../assets/sim_data";
 
+// HTML area for the presents simulation.
 const canvas = document.querySelector<HTMLCanvasElement>('#campaign-gifts-pile');
 if (canvas === null) {
   throw new Error("present_for_blender could not find a canvas element with id 'campaign-gifts-pile'");
 }
+const HEIGHT = canvas.offsetHeight / 1;
 
-/*const colors: Array<vec4> = [
-    [0.839, 0.761, 0.839, 1.0], // 0xd6c2d6
-    [0.239, 0.302, 0.361, 1.0], // 0x3d4d5c
-    [0.459, 0.549, 0.639, 1.0], // 0x758ca3
-    [1.000, 0.820, 0.102, 1.0], // 0xffd11a
-    [0.820, 0.820, 0.878, 1.0], // 0xd1d1e0
-    [1.000, 0.651, 0.302, 1.0], // 0xffa64d
-];*/
+// Data - the presents!
+const masses = [5, 10, 25, 50, 100, 250]; // in euros :)
+
+// Simulation Configuration
+
 const colors: Array<vec4> = simData.presentColors.map((v) => hexToRGBFloat(v));
 // Bg color to hardcode in the fragment shader.
 //console.log(hexToRGBFloat(simData.colorBg));
 
-const masses = [5, 10, 25, 50, 100, 250]; // in euros :)
-const masses_kg =  masses.map((v) => v / 100.0);
+const scale = simData.scale;
+const widthVariancePercent = simData.widthVariancePercent;
+//let widthVariancePercent = [0.0, 0.7, 0.3, 0.5, 0.25, 0.1]; // How much longer is one side vs the other.
+                                                             // e.g. 0.5 = one side is 50% longer than the other.
+// let widthVariancePercent = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]; // Use this one for squares.
 
+// ~ end Configuration
+
+// Calculate present sizes in px.
 const m_to_px = 100;
 const px_to_m = 1 / m_to_px;
-const scale = simData.scale;
 const sidesPx = masses.map((v) => Math.sqrt(v) * scale);
-// const widthVariancePercent = [0.0, 0.7, 0.3, 0.5, 0.25, 0.1]; // How much longer is one side vs the other.
-                                                            // e.g. 0.5 = one side is 50% longer than the other.
-// const widthVariancePercent = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]; // Use this one for squares.
-const widthVariancePercent = simData.widthVariancePercent;
 const widthsPx =  sidesPx.map((v, i) => v + v * widthVariancePercent[i]);
 const heightsPx = widthsPx.map((v, i) => sidesPx[i]*sidesPx[i] / v);
+//console.log("Present widths in px:", widthsPx);
 
-let ids: Array<number> = [];
-let positions: Array<vec2> = [];
-let orientations: Array<vec2> = [];
-let velocities: Array<vec2> = [];
-let accells: Array<vec2> = [];
+// Runtime data for the present simulation and present renderer.
+const masses_kg =  masses.map((v) => v / 100.0);
+const gravity = -9.81; // m/s2
+
+class PresentSimData {
+  presentID = 0;    // Index into the presents. It uniquely ids a present for the sim and is returned for the tooltip.
+  tierIdx = 0;      // Index into the subscription tiers configurations (labels, box color, size, etc...).
+  pos: vec2 = [0.0, 0.0]; // Current position of the center of each present. In physics sim coordinates.
+  ori: vec2 = [0.0, 1.0]; // Current orientation of each present, as a normalized heading vector with Y-up being forward.
+  v:   vec2 = [0.0, 0.0];
+  a:   vec2 = [0.0, 0.0];
+}
+const presentSimData = new Array<PresentSimData>();
 
 class Contact {
   toi: number;
@@ -53,68 +62,75 @@ class Contact {
     this.surface_normal = surface_normal;
   }
 }
-
 let contacts: Array<Array<Contact>> = [];
 
-const gravity = -9.81; // m/s2
+generate_initial_positions();
+//load_initial_positions();
 
 function load_initial_positions()
 {
-  for (const ob of simData.presents) {
-    if (ob.label === 'border')
+  for (const body of simData.presents) {
+    if (body.label === 'border')
       continue;
 
-    // Convert the information in the label to the indexes of the structures here.
-    const tierValue = Number(ob.label.split("-")[0]);
-    const tierIdx = masses.indexOf(tierValue);
+    var present = new PresentSimData();
+
+    // Convert the information in the label to the indexes.
+    const labelParts = body.label.split("-");
+    present.presentID = Number(labelParts[1]);
+    present.tierIdx = masses.indexOf(Number(labelParts[0]));
 
     // Create an object and its initial simulation values.
     // Convert from matter.js (0,0) at top-left with y down and angles along x
     // to (0,0) at bottom-left with y up with angles along y.
-    positions.push([ob.position.x * px_to_m, (canvas.height - ob.position.y) * px_to_m]);
-    velocities.push([0.0, 0.0]);
-    orientations.push([Math.sin(ob.angle), Math.cos(ob.angle)]);
-    ids.push(tierIdx);
+    present.pos = [body.position.x * px_to_m, (HEIGHT - body.position.y) * px_to_m];
+    present.ori = [Math.sin(body.angle), Math.cos(body.angle)];
+
+    presentSimData.push(present);
     contacts.push(new Array());
   }
+  
 }
 
 function generate_initial_positions()
 {
   let y = 300;
   let x = 10;
-  for (let tier = 0; tier < 1; tier++) {
-    const radius = widthsPx[tier] / 2;
-    x += radius;
-    positions.push([x * px_to_m, y * px_to_m]);
-    velocities.push([0.0, -1]);
-    orientations.push([0.5, 0.5]);
-    accells.push([0.0, gravity]);
-    ids.push(tier);
-    contacts.push(new Array());
+  let tier = 3;
+  let p = 0;
+  const radius = widthsPx[tier] / 2;
+  x += radius;
 
-    positions.push([x * px_to_m, 100 * px_to_m]);
-    velocities.push([0.0, 1.0]);
-    orientations.push([0, 1]);
-    accells.push([0.0, 0.0]);
-    ids.push(tier);
-    contacts.push(new Array());
+  var present = new PresentSimData();
+  present.presentID = p++;
+  present.tierIdx = tier;
+  present.pos = [x * px_to_m, y * px_to_m];
+  present.ori = [0, -1];
+  present.a = [0.0, gravity];
+  presentSimData.push(present);
+  contacts.push(new Array());
 
-    x += radius + 15;
-  }
-  console.log("positions in meters", positions);
+  y = 100;
+  present = new PresentSimData();
+  present.presentID = p++;
+  present.tierIdx = tier;
+  present.pos = [x * px_to_m, y * px_to_m];
+  present.ori = [0, -1];
+  present.a = [0.0, 0.0];
+  presentSimData.push(present);
+  contacts.push(new Array());
 }
 
 function check_intersections() {
-  for (let i = 0; i < positions.length; i++) {
-    for (let j = i+1; j < positions.length; j++) {
+  for (let i = 0; i < presentSimData.length; i++) {
+    for (let j = i+1; j < presentSimData.length; j++) {
 
-      const tier1 = ids[i];
-      const tier2 = ids[j];
+      const tier1 = presentSimData[i].tierIdx;
+      const tier2 = presentSimData[j].tierIdx;
       const radius1_m = heightsPx[tier1] * 0.5 * px_to_m;
       const radius2_m = heightsPx[tier2] * 0.5 * px_to_m;
-      const pos1 = positions[i];
-      const pos2 = positions[j];
+      const pos1 = presentSimData[i].pos;
+      const pos2 = presentSimData[j].pos;
 
       if (pos1[1] - pos2[1] < radius1_m + radius2_m)
       {
@@ -133,7 +149,7 @@ function determine_forces(i: number) {
   for (let c = 0; c < contacts[i].length; c++) {
     const contact = contacts[i][c];
     const i2 = contact.other_body_id;
-    net_force += -b * velocities[i2][1];
+    net_force += -b * presentSimData[i2].v[1];
     console.log("net force", i, net_force);
   }
   return net_force;
@@ -145,15 +161,15 @@ function update_physics(delta_time_ms: number) {
   check_intersections();
 
   // Update physics.
-  for (let i = 0; i < positions.length; i++) {
-    const tier = ids[i];
+  for (let i = 0; i < presentSimData.length; i++) {
+    const tier = presentSimData[i].tierIdx;
     const half_height_m = heightsPx[tier] * 0.5 * px_to_m;
     const mass = masses_kg[tier];
 
     // Calculate only on y to not bother with multiplying "vectors" in JS.
-    const prev_p = positions[i][1];
-    const prev_v = velocities[i][1];
-    const prev_a = accells[i][1];
+    const prev_p = presentSimData[i].pos[1];
+    const prev_v = presentSimData[i].v[1];
+    const prev_a = presentSimData[i].a[1];
 
     const f_net = determine_forces(i);
 
@@ -164,15 +180,15 @@ function update_physics(delta_time_ms: number) {
     const new_v = half_stepped_v + 0.5*new_a*delta_time_s;
 
     if (new_p > half_height_m) {
-      positions[i][1] = new_p;
-      velocities[i][1] = new_v;
-      accells[i][1] = new_a;
+      presentSimData[i].pos[1] = new_p;
+      presentSimData[i].v[1] = new_v;
+      presentSimData[i].a[1] = new_a;
       console.log("p: ", new_p.toFixed(2), "v: ", new_v.toFixed(2), " a: ", new_a.toFixed(2));
     } else {
       // Give it a rest.
-      positions[i][1] = half_height_m;
-      velocities[i][1] = 0.0;
-      accells[i][1] = 0.0;
+      presentSimData[i].pos[1] = half_height_m;
+      presentSimData[i].v[1] = 0.0;
+      presentSimData[i].a[1] = 0.0;
     }
   }
 }
@@ -182,22 +198,21 @@ function draw() {
   const ui = uiRenderer;
   ui.beginFrame();
 
-  for (let i = 0; i < positions.length; i++) {
-    const tier = ids[i];
-    const p_px : vec2 = [ positions[i][0] * m_to_px, positions[i][1] * m_to_px ];
-    ui.addOrientedRect(p_px, orientations[i], widthsPx[tier], heightsPx[tier], colors[tier], tier);
-    //ui.addFrame(p_px[0]-widthsPx[tier]*0.5, p_px[1]-heightsPx[tier]*0.5, widthsPx[tier], heightsPx[tier], 1, [1,0,0,1]);
+  for (let i = 0; i < presentSimData.length; i++) {
+    const tier = presentSimData[i].tierIdx;
+    const p_px : vec2 = [ presentSimData[i].pos[0] * m_to_px, presentSimData[i].pos[1] * m_to_px ];
+    ui.addOrientedRect(p_px, presentSimData[i].ori, widthsPx[tier], heightsPx[tier], colors[tier], tier);
   }
   // Draw body origins.
   /*{
     const axisSize = 10;
-    for (let i = 0; i < positions.length; i++) {
-      const p_px : vec2 = [ positions[i][0] * m_to_px, positions[i][1] * m_to_px ];
-      ui.addLine(p_px, [p_px[0]+orientations[i][1]*axisSize, p_px[1]-orientations[i][0]*axisSize], 1, [1.0, 0.0, 0.0, 1.0]);
+    for (const present of presentSimData) {
+      const p_px = [ present.pos[0] * m_to_px, present.pos[1] * m_to_px ];
+      ui.addLine(p_px, [p_px[0]+present.ori[1]*axisSize, p_px[1]-present.ori[0]*axisSize], 1, [1.0, 0.0, 0.0, 1.0]);
     }
-    for (let i = 0; i < positions.length; i++) {
-      const p_px : vec2 = [ positions[i][0] * m_to_px, positions[i][1] * m_to_px ];
-      ui.addLine(p_px, [p_px[0]+orientations[i][0]*axisSize, p_px[1]+orientations[i][1]*axisSize], 1, [0.0, 1.0, 0.0, 1.0]);
+    for (const present of presentSimData) {
+      const p_px = [ present.pos[0] * m_to_px, present.pos[1] * m_to_px ];
+      ui.addLine(p_px, [p_px[0]+present.ori[0]*axisSize, p_px[1]+present.ori[1]*axisSize], 1, [0.0, 1.0, 0.0, 1.0]);
     }
   }*/
 
@@ -228,9 +243,6 @@ function tick_simulation(current_time: number) {
     requestAnimationFrame(tick_simulation);
 }
 
-//generate_initial_positions();
-load_initial_positions();
 const uiRenderer: UIRenderer = new UIRenderer(canvas, draw);
-const patternsTextureID: WebGLTexture = uiRenderer.loadImage('/assets/patterns.png');
-
+uiRenderer.loadImage('/assets/patterns.png');
 tick_simulation(frame_dur);
